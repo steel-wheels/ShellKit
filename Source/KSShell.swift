8ff2ec7024eb08c2f3f5f40b56bacd2400bb01c0
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2026-03-08 21:14:43 +0900

    KSShell: Initial commit

---
+++ Source/KSShell.swift
@@ -0,0 +1,14 @@
+/*
+ * @file KSShell.swift
+ * @description Define MIEscapeCode
+ * @par Copyright
+ *   Copyright (C) 2025 Steel Wheels Project
+ */
+
+import Foundation
+
+public class KSShell
+{
+
+}
+
--- Source/ShellKit.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ShellKit.swift
-//  ShellKit
-//
-//  Created by Tomoo Hamada on 2026/03/08.
-//
-
-import Foundation
-
